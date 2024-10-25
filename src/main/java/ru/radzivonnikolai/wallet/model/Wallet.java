609358f0ccaf19800bb01168c4223481581b51af
Nikolay <Novizdarialokin@yandex.ru>
2024-10-25 18:51:40 +0600

    feat: Добавил модель Wallet

---
+++ src/main/java/ru/radzivonnikolai/wallet/model/Wallet.java
@@ -0,0 +1,19 @@
+package ru.radzivonnikolai.wallet.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "wallet")
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+}
