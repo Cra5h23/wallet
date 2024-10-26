3abdfb691785741c1d09ba7c67de24c01f78666d
Nikolay <Novizdarialokin@yandex.ru>
2024-10-26 17:46:07 +0600

    feat: Добавил коментарии

--- src/main/java/ru/radzivonnikolai/wallet/controller/WalletController.java
+++ src/main/java/ru/radzivonnikolai/wallet/controller/WalletController.java
@@ -1,8 +1,13 @@
 package ru.radzivonnikolai.wallet.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.radzivonnikolai.wallet.dto.WalletRequestDto;
 import ru.radzivonnikolai.wallet.service.WalletService;
@@ -17,25 +22,51 @@ import java.util.UUID;
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
 @Slf4j
+@Validated
 public class WalletController {
     private final WalletService service;
 
+    /**
+     * Метод для обновления баланса кошелька.
+     *
+     * @param dto объект DTO с данными для обновления баланса
+     * @return ответ с кодом состояния HTTP и телом ответа
+     */
     @PostMapping("/wallet")
-    public ResponseEntity<Object> updateBalance(@RequestBody WalletRequestDto dto) {
+    public ResponseEntity<Object> updateBalance(@Valid @RequestBody WalletRequestDto dto) {
         log.info("Получен запрос: POST /api/v1/wallet body={}", dto);
+
         return ResponseEntity
-                .ok(service.updateBalance(dto));
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.updateBalance(dto));
     }
 
+    /**
+     * Метод для получения кошелька по id.
+     *
+     * @param walletId id кошелька
+     * @return ответ с кодом состояния HTTP и телом ответа
+     */
     @GetMapping("/wallets/{WALLET_UUID}")
-    public ResponseEntity<Object> getWallet(@PathVariable(value = "WALLET_UUID") UUID walletId) {
+    public ResponseEntity<Object> getWallet(@PathVariable(value = "WALLET_UUID") @NotNull UUID walletId) {
+        log.info("Получен запрос: GET /api/v1/wallets/{}", walletId);
+
         return ResponseEntity
                 .ok(service.getWallet(walletId));
     }
 
-    @PostMapping()
+    /**
+     * Метод для создания нового кошелька.
+     *
+     * @return ответ с кодом состояния HTTP и телом ответа
+     */
+    @PostMapping
     public ResponseEntity<Object> createWallet() {
+        log.info("Получен запрос: POST /api/v1");
+
         return ResponseEntity
-                .ok(service.createWallet());
+                .status(HttpStatus.CREATED)
+                .body(service.createWallet());
     }
 }
